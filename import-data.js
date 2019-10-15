7d54f45005799d7683a3dcded04954b62925bce5
Aaron Krajeski <ak@aaronk.is>
2019-10-14 22:14:50 -0400

    Play with importing from sheets

---
+++ import-data.js
@@ -0,0 +1,97 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '9234134087-lpj7vqv36h37s01uafoslo7o7o17ec6j.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBAoeJQ6Sbs_-tjG7aVW4TpD8E-ciHGWOc';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    listMajors();
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+outputData = [];
+function listMajors() {
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1dlaWXhgQ1O1Mc9qkUiRA0OfEgT-OsqrYatltgypJJvU',
+    range: 'Sheet1!A1:G',
+  }).then(function(response) {
+    var values = response.result.values;
+    var headers = values.shift();
+    values.forEach((row) => {
+      entry = {}
+      row.forEach((value, index) => {
+        entry[headers[index]] = value;
+      });
+      outputData.push(entry);
+    });
+    console.log(outputData);
+  }, function(failure) {
+    console.log('Error: ' + failure.result.error.message);
+  });
+}
