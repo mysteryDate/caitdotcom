a724ea7a8b1dac7990baf56c4fb45968704b5183
Aaron Krajeski <krajeski@gmail.com>
2021-07-12 12:06:41 -0400

    Just use PIL, this is so much better and easier

--- image-resize.py
+++ image-resize.py
@@ -1,5 +1,4 @@
-from skimage.transform import resize
-from skimage.io import imread, imsave
+from PIL import Image
 import os, math
 
 IMAGE_FOLDER = "images"
@@ -10,16 +9,18 @@ input_images = os.listdir(IMAGE_FOLDER)
 output_images = os.listdir(OUTPUT_FOLDER)
 
 for image_file_name in input_images:
-  print(image_file_name)
-  if image_file_name in output_images:
+  if ".".join([image_file_name, "jpg"]) in output_images:
     continue
+  print(image_file_name)
   try:
-    img = imread(os.path.join(IMAGE_FOLDER, image_file_name))
+    img = Image.open(os.path.join(IMAGE_FOLDER, image_file_name)).convert("RGB")
   except ValueError:
     print("Can't read in {}".format(image_file_name))
-  height, width, dim = img.shape
+  width, height = img.size
   h_ratio = MAX_OUTPUT_DIMENSION / height
   w_ratio = MAX_OUTPUT_DIMENSION / width
   min_ratio = min(1, min(h_ratio, w_ratio))
-  small_image = resize(img, (math.floor(min_ratio * height), math.floor(min_ratio * width)))
-  imsave(os.path.join(OUTPUT_FOLDER, ".".join([image_file_name, "jpg"])), small_image[:,:,:3])
+  newsize = (math.floor(min_ratio * width), math.floor(min_ratio * height))
+  small_image = img.resize(newsize)
+  output_filename = os.path.join(OUTPUT_FOLDER, ".".join([image_file_name, "jpg"]))
+  small_image.save(output_filename, format="jpeg")
